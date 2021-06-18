eca075f9c0a5ead20df39fb8329b6739c5e62f6f
Kevin Rademacher <krademacher@users.noreply.github.com>
2021-06-18 09:54:59 +0200

    Started on entity framework

---
+++ EFData/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Core.DomainModel;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFData
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Animal> Animals { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Treatment> Treatments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // TODO: Add relations
+        }
+    }
+}
